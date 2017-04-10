b6b15b17a04dc3ccf7147bd783f97a1bdaac376a
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2017-04-10 11:45:52 -0700

    Added bubbleView to the chat message cell

--- ChatApp/ChatMessageCell.swift
+++ ChatApp/ChatMessageCell.swift
@@ -13,14 +13,29 @@ class ChatMessageCell: UICollectionViewCell {
     let textView: UITextView = {
         let tv = UITextView()
         tv.font = UIFont.systemFont(ofSize: 16)
+        tv.backgroundColor = UIColor.clear
         tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.textColor = .white
         return tv
+    }()
+    
+    let bubbleView: UIView = {
+        let bv = UIView()
+        bv.backgroundColor = UIColor(r: 232, g: 85, b: 102)
+        bv.translatesAutoresizingMaskIntoConstraints = false
+        return bv
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        addSubview(bubbleView)
         addSubview(textView)
+        
+        bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
         
         textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
